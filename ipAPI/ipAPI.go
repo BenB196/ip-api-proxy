e1e3b7fd15ac48b5d0b08d01fa0c80204d2a2e43
BenB196 <benbrenek@yahoo.com>
2019-09-05 20:32:50 -0400

    Added comments to validate functions

--- ipAPI/ipAPI.go
+++ ipAPI/ipAPI.go
@@ -12,6 +12,10 @@ var AllowedLangs = []string{"en","de","es","pt-BR","fr","ja","zh-CN","ru"}
 
 var IPDNSRegexp = regexp.MustCompile(`((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}|(([a-zA-Z])|([a-zA-Z][a-zA-Z])|([a-zA-Z][0-9])|([0-9][a-zA-Z])|([a-zA-Z0-9][a-zA-Z0-9-_]{1,61}[a-zA-Z0-9]))\.([a-zA-Z]{2,6}|[a-zA-Z0-9-]{2,30}\.[a-zA-Z]{2,3}))`)
 
+/*
+ValidateFields - validates the fields string to make sure it only has valid parameters
+fields - string of comma separated values
+ */
 func ValidateFields(fields string) (string, error) {
 	fieldsSlice := strings.Split(fields,",")
 
@@ -24,6 +28,10 @@ func ValidateFields(fields string) (string, error) {
 	return fields, nil
 }
 
+/*
+ValidateLang - validates the lang string to make sure it is a valid lang option
+lang - string with lang value
+ */
 func ValidateLang(lang string) (string, error) {
 	if !contains(AllowedLangs,lang) {
 		return "", errors.New("error: illegal lang value provided: " + lang)
@@ -32,6 +40,14 @@ func ValidateLang(lang string) (string, error) {
 	return lang, nil
 }
 
+/*
+contains - checks a string slice to see if it contains a string
+slice - string slice which you want to check
+item - string which you want to see if exists in the string slice
+
+returns
+bool - true if slice contains string, else false
+ */
 func contains(slice []string, item string) bool {
 	for _, value := range slice {
 		if value == item {
